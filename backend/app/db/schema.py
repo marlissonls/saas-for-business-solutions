d831c237626282c68eb8ca418c8bf7a94991c5aa
bruno-brn <123398572+bruno-brn@users.noreply.github.com>
2023-12-05 15:25:53 -0300

    adicionado schema do banco de dados

--- backend/app/db/schema.py
+++ backend/app/db/schema.py
@@ -1,19 +1,56 @@
-from sqlalchemy import create_engine, Column, String, Integer, ForeignKey
-from sqlalchemy.orm import DeclarativeBase, relationship
+from sqlalchemy import Column, Integer, String, Text, DateTime, ForeignKey
+from sqlalchemy.sql import func
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
 from app.config import engine
 
-class Base(DeclarativeBase):
-    pass
+Base = declarative_base()
 
-class UserSchema(Base):
+class Company(Base):
+    __tablename__ = 'companies'
+    id = Column(Integer, primary_key=True)
+    nome = Column(String(255), nullable=False)
+    descricao = Column(Text)
+    createdAt = Column(DateTime(timezone=True), server_default=func.now())
+    updatedAt = Column(DateTime, default=None)
+    deletedAt = Column(DateTime, default=None)
 
-    __tablename__ = "users"
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    nome = Column(String(255), nullable=False)
+    email = Column(String(255), nullable=False)
+    senha = Column(String(255), nullable=False)
+    empresa_id = Column(Integer, ForeignKey('companies.id'))
+    createdAt = Column(DateTime(timezone=True), server_default=func.now())
+    updatedAt = Column(DateTime, default=None)
+    deletedAt = Column(DateTime, default=None)
+    empresa = relationship('Company', back_populates='users') 
 
-    id = Column(String, primary_key=True)
-    name = Column(String(30))
-    email = Column(String)
-    password = Column(String)
-    profile_image = Column(String)
+class Dashboard(Base):
+    __tablename__ = 'dashboards'
+    id = Column(Integer, primary_key=True)
+    nome = Column(String(255), nullable=False)
+    descricao = Column(Text)
+    empresa_id = Column(Integer, ForeignKey('companies.id'))
+    createdAt = Column(DateTime(timezone=True), server_default=func.now())
+    updatedAt = Column(DateTime, default=None)
+    deletedAt = Column(DateTime, default=None)
+    empresa = relationship('Company', back_populates='dashboards')  
 
+class Model(Base):
+    __tablename__ = 'models'
+    id = Column(Integer, primary_key=True)
+    nome = Column(String(255), nullable=False)
+    descricao = Column(Text)
+    empresa_id = Column(Integer, ForeignKey('companies.id'))
+    createdAt = Column(DateTime(timezone=True), server_default=func.now())
+    updatedAt = Column(DateTime, default=None)
+    deletedAt = Column(DateTime, default=None)
+    empresa = relationship('Company', back_populates='models')  
+
+Company.users = relationship('User', back_populates='empresa')
+Company.dashboards = relationship('Dashboard', back_populates='empresa')
+Company.models = relationship('Model', back_populates='empresa')
 
 Base.metadata.create_all(engine)
--- backend/app/repository/user/sqlalchemy/__init__.py
+++ backend/app/repository/user/sqlalchemy/__init__.py
@@ -1,30 +1,30 @@
 from app.repository.user.models.repository_interface import IUserRepository
-from app.db.schema import UserSchema
+from app.db.schema import User
 from sqlalchemy.orm import Session
 from typing import List
 
 
 class UserRepository(IUserRepository):
 
-    def get_user_by_id_repository(self, user_id: str, session: Session) -> UserSchema | None:
-        return session.query(UserSchema).filter(UserSchema.id == user_id).first()
+    def get_user_by_id_repository(self, user_id: str, session: Session) -> User | None:
+        return session.query(User).filter(User.id == user_id).first()
 
 
-    def get_users_repository(self, session: Session) -> List[UserSchema]:
-        return session.query(UserSchema).all()
+    def get_users_repository(self, session: Session) -> List[User]:
+        return session.query(User).all()
     
 
-    def get_user_by_email_repository(self, email: str, session: Session) -> UserSchema | None:
-        return  session.query(UserSchema).filter(UserSchema.email == email).first()
+    def get_user_by_email_repository(self, email: str, session: Session) -> User | None:
+        return  session.query(User).filter(User.email == email).first()
         
 
-    def create_user_repository(self, user: UserSchema, session: Session) -> None:
+    def create_user_repository(self, user: User, session: Session) -> None:
         session.add(user)
 
 
-    def update_user_repository(self, user: UserSchema, session: Session) -> None:
+    def update_user_repository(self, user: User, session: Session) -> None:
         session.merge(user)
 
 
-    def delete_user_repository(self, user: UserSchema, session: Session) -> None:
+    def delete_user_repository(self, user: User, session: Session) -> None:
         session.delete(user)
