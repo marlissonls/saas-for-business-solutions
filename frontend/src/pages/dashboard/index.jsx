b6fdccdf60aa2e44fbe81b849189e99fcfe3759d
Marlisson Silva <marlisson.business@gmail.com>
2023-12-07 16:13:55 -0300

    add página de dashboard

--- frontend/src/pages/dashboard/index.jsx
+++ frontend/src/pages/dashboard/index.jsx
@@ -1,20 +1,24 @@
 import React, { useState, useEffect }from "react";
 import { useParams } from "react-router-dom";
+
 import api from "../../services/api";
+import SideBar from "../../components/sideBar";
 
 import Dash1 from "./dashInterfaces/dash1";
 import Dash2 from "./dashInterfaces/dash2";
+import MainContent from "../../components/mainContent";
+
 
 function selectDash(id, data) {
     switch (id) {
         case 1: return <Dash1 data={ data }/>
-        case 2: return <Dash2 />
+        case 2: return <Dash2 data={ data }/>
         default: return null
     }
 }
 
 async function getDashboard(id) {
-    const response = await api.get(`/.../${id}`) // VER A ROTA MEU AMIGO
+    const response = await api.get(`/dashboards/${id}`) // VER A ROTA MEU AMIGO
     return response.data
 }
 
@@ -28,9 +32,12 @@ function Dashboard(props) {
         }
         updateData()
     }, [id])
-    return <>{
-        data ? selectDash(id, data) : <div>Buscando os dados...</div>
-    }</>
+    return <div className='body'>
+        <SideBar />
+        <MainContent>
+            { data ? selectDash(id, data) : <div>Buscando os dados...</div> }
+        </MainContent>
+    </div>
 }
 
 export default Dashboard;
