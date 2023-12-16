2968adba7fed965ec05641cdd437406d3425e9ba
bruno-brn <123398572+bruno-brn@users.noreply.github.com>
2023-12-16 00:12:15 -0300

    adicionado arquivo sqlachemy no endpoint model

--- backend/app/repository/mlmodels/sqlalchemy/__init__.py
+++ backend/app/repository/mlmodels/sqlalchemy/__init__.py
@@ -1,24 +1,25 @@
-from app.repository.company.models.repository_interface import ICompanyRepository
-from app.db.schema import Company
+from app.repository.mlmodels.models.repository_interface import IModelRepository
+from app.db.schema import Model
 from sqlalchemy.orm import Session
 from sqlalchemy import and_
 from typing import List
 
 
-class CompanyRepository(ICompanyRepository):
+class ModelRepository(IModelRepository):
 
-    def get_company_by_id_repository(self, company_id: str, session: Session) -> Company | None:
-        return session.query(Company).filter(and_(Company.id == company_id, Company.deleted_at == None)).first()
+    def get_model_by_id_repository(self, model_id: str, session: Session) -> Model | None:
+        return session.query(Model).filter(and_(Model.id == model_id, Model.deleted_at == None)).first()
 
-    def get_companies_repository(self, session: Session) -> List[Company] | List:
-        return session.query(Company).filter(Company.deleted_at == None).all()
+    def get_models_repository(self, session: Session) -> List[Model] | List:
+        return session.query(Model).filter(Model.deleted_at == None).all()
 
-    def create_company_repository(self, company: Company, session: Session) -> None:
-        session.add(company)
+    def create_model_repository(self, model: Model, session: Session) -> None:
+        session.add(model)
 
-    def update_company_repository(self, company: Company, session: Session) -> None:
-        session.merge(company)
+    def update_model_repository(self, model: Model, session: Session) -> None:
+        session.merge(model)
 
-    def delete_company_repository(self, company: Company, session: Session) -> None:
-        session.merge(company)
+    def delete_model_repository(self, model: Model, session: Session) -> None:
+        session.merge(model)
+
 
