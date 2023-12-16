4f3091707a584a6868af0ef200fb555e56493bda
Marlisson Silva <marlisson.business@gmail.com>
2023-12-16 09:46:23 -0300

    chore: remove comments

--- backend/app/middlewares/__init__.py
+++ backend/app/middlewares/__init__.py
@@ -1,5 +1,2 @@
-# Definir middleware de autenticação
-# Definir middleware de acesso ao bando de dados
-
 from app.middlewares.database import db_session_middleware
 from starlette.middleware.base import BaseHTTPMiddleware
