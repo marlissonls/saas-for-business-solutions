d6edbfd5e268a1a9c39b687e1266775b624b20f8
bruno-brn <123398572+bruno-brn@users.noreply.github.com>
2023-12-16 00:16:01 -0300

    adicionado arquivo router  no endpoint model

--- backend/app/router/mlmoldel_router.py
+++ backend/app/router/mlmoldel_router.py
@@ -1,75 +1,66 @@
-from fastapi import APIRouter, Form, File, UploadFile, status, Depends, Request
-from app.repository.company.models.company_models import PutCompany, GetCompanyResponse, RegisterCompanyResponse, GetCompanyData
-from app.repository.company.sqlalchemy import CompanyRepository
-from app.repository.company.controller import CompanyController
-from app.repository.company.service import CompanyService
+from fastapi import APIRouter, Form, status, Depends, UploadFile
+from app.repository.mlmodels.models.model_models import PutModel, GetModelResponse, RegisterModelResponse
+from app.repository.mlmodels.sqlalchemy import ModelRepository
+from app.repository.mlmodels.controller import ModelController
+from app.repository.mlmodels.service import ModelService
 from app.utils.auth import get_authenticated_user
-from typing import Any, Annotated, Optional
 from sqlalchemy.orm import Session
-import logging
+from typing import Any, Annotated, Optional
 
-repository = CompanyRepository()
-service = CompanyService(repository)
-controller = CompanyController(service)
+repository = ModelRepository()
+service = ModelService(repository)
+controller = ModelController(service)
 
-router = APIRouter(prefix="/company", tags=["company"])
+router = APIRouter(prefix="/model", tags=["model"])
 
 def get_db(request: Request):
     return request.state.db
 
-@router.get('/{company_id}', status_code=status.HTTP_200_OK, response_model=GetCompanyResponse)
-def get_company_by_id(company_id: str, current_user: dict = Depends(get_authenticated_user), session: Session = Depends(get_db)) -> Any:
+@router.get('/{model_id}', status_code=status.HTTP_200_OK, response_model=GetModelResponse)
+def get_model_by_id(model_id: str, current_user: dict = Depends(get_authenticated_user), session: Session = Depends(get_db)) -> Any:
     if current_user['role'] == 'admin':
-        return controller.get_company_by_id_controller(company_id, session)
+        return controller.get_model_by_id_controller(model_id, session)
     else:
-        return GetCompanyResponse(
+        return GetModelResponse(
             status=False,
             message='Acesso negado.',
             data=None,
         )
 
-
-@router.get('/', status_code=status.HTTP_200_OK, response_model=GetCompanyResponse)
-def get_companies(current_user: dict = Depends(get_authenticated_user), session: Session = Depends(get_db)) -> Any:
+@router.get('/', status_code=status.HTTP_200_OK, response_model=GetModelResponse)
+def get_models(current_user: dict = Depends(get_authenticated_user), session: Session = Depends(get_db)) -> Any:
     if current_user['role'] == 'admin':
-        return controller.get_companies_controller(session)
+        return controller.get_models_controller(session)
     else:
-        return GetCompanyResponse(
+        return GetModelResponse(
             status=False,
             message='Acesso negado.',
             data=None,
         )
 
-
-@router.post('/register', status_code=status.HTTP_201_CREATED, response_model=RegisterCompanyResponse)
-def create_company(
+@router.post('/register', status_code=status.HTTP_201_CREATED, response_model=RegisterModelResponse)
+def create_model(
     name: Annotated[str, Form()],
-    area: Annotated[str, Form()],
     description: Annotated[str, Form()],
-    localization: Annotated[str, Form()],
     session: Session = Depends(get_db)
 ) -> Any:
-    return controller.create_company_controller(
+    return controller.create_model_controller(
         name,
-        area,
         description,
-        localization,
         session
     )
 
-@router.put('/{company_id}', status_code=status.HTTP_200_OK, response_model=GetCompanyResponse)
-def update_company(
-    company_id: str,
+@router.put('/{model_id}', status_code=status.HTTP_200_OK, response_model=GetModelResponse)
+def update_model(
+    model_id: str,
     name: Optional[str] = Form(None),
-    area: Optional[str] = Form(None),
     description: Optional[str] = Form(None),
-    localization: Optional[str] = Form(None),
     current_user: dict = Depends(get_authenticated_user),
     session: Session = Depends(get_db)
 ) -> Any:
-    company_data = PutCompany(name=name, area=area, description=description, localization=localization)
-    return controller.update_company_controller(company_id, company_data, session)
+    model_data = PutModel(name=name, description=description)
+    return controller.update_model_controller(model_id, model_data, session)
 
-@router.delete('/{company_id}', status_code=status.HTTP_204_NO_CONTENT)
-def delete_company(company_id: str, current_user: dict = Depends(get_authenticated_user), session: Session = Depends(get_db)) -> None:
-    controller.delete_company_controller(company_id, session)
+@router.delete('/{model_id}', status_code=status.HTTP_204_NO_CONTENT)
+def delete_model(model_id: str, current_user: dict = Depends(get_authenticated_user), session: Session = Depends(get_db)) -> None:
+    controller.delete_model_controller(model_id, session)
