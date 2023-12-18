f09a54a45a4091957b81f51d28aaf7fc3c0a0aa9
bruno-brn <123398572+bruno-brn@users.noreply.github.com>
2023-12-18 18:00:05 -0300

    adcionado ajuste no baseModel

---
+++ backend/app/repository/dashboards/models/dashboard_model.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel
+from typing import Optional, List, Union
+
+class GetDashboardId(BaseModel):
+    id: str
+
+class PostDashboard(BaseModel):
+    name: str
+    description: str
+
+class GetDashboardData(BaseModel):
+    id: str
+    name: str
+    description: str
+    company_id: str
+
+class GetDashboardResponse(BaseModel):
+    status: bool
+    message: str
+    data: Union[GetDashboardData, List[GetDashboardData], None] = None
+
+class PutDashboard(BaseModel):
+    name: Optional[str] = None
+    description: Optional[str] = None
+
+class RegisterDashboardResponse(BaseModel):
+    status: bool
+    message: str
+    data: GetDashboardId | None
