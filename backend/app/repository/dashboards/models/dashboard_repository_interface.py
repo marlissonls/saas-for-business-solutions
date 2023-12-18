42a6c58d257aa9e47a02b548a42cf13c49e64872
bruno-brn <123398572+bruno-brn@users.noreply.github.com>
2023-12-18 18:08:03 -0300

    adcionado ajuste no dashboard_repository

---
+++ backend/app/repository/dashboards/models/dashboard_repository_interface.py
@@ -0,0 +1,28 @@
+from app.db.schema import Dashboard
+from app.repository.dashboard.dashboard_models import PutDashboard
+from sqlalchemy.orm import Session
+from abc import ABC, abstractmethod
+from typing import List
+
+
+class IDashboardRepository(ABC):
+
+    @abstractmethod
+    def get_dashboard_by_id_repository(self, dashboard_id: str, session: Session) -> Dashboard | None:
+        pass
+
+    @abstractmethod
+    def get_dashboards_repository(self, session: Session) -> List[Dashboard] | list:
+        pass
+    
+    @abstractmethod
+    def create_dashboard_repository(self, dashboard: Dashboard, session: Session) -> None:
+        pass
+
+    @abstractmethod
+    def update_dashboard_repository(self, dashboard: Dashboard, session: Session) -> None:
+        pass
+
+    @abstractmethod
+    def delete_dashboard_repository(self, dashboard: Dashboard, session: Session) -> None:
+        pass
