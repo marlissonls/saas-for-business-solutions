116f6f26cae683f88df82b1e9f045ba0e57b1572
bruno-brn <123398572+bruno-brn@users.noreply.github.com>
2023-12-18 19:10:12 -0300

    adcionado arquivo controller_interface

---
+++ backend/app/repository/dashboards/models/dashboard_controller_interface.py
@@ -0,0 +1,31 @@
+from app.repository.dashboard.dashboard_model import GetDashboardResponse, RegisterDashboardResponse, PutDashboard
+from sqlalchemy.orm import Session
+from abc import ABC, abstractmethod
+
+class IDashboardController(ABC):
+
+    @abstractmethod
+    def get_dashboard_by_id_controller(self, dashboard_id: str, session: Session) -> GetDashboardResponse:
+        pass
+    
+    @abstractmethod
+    def get_dashboards_controller(self, session: Session) -> GetDashboardResponse:
+        pass
+
+    @abstractmethod
+    def create_dashboard_controller(
+        self, 
+        name: str,
+        description: str,
+        company_id: str,
+        session: Session
+    ) -> RegisterDashboardResponse:
+        pass
+
+    @abstractmethod
+    def update_dashboard_controller(self, dashboard_id: str, dashboard: PutDashboard, session: Session) -> GetDashboardResponse:
+        pass
+
+    @abstractmethod
+    def delete_dashboard_controller(self, dashboard_id: str, session: Session) -> GetDashboardResponse:
+        pass
