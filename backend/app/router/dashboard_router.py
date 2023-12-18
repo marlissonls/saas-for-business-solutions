7e3f37e8a9c13ba3b46976157e6fe47bff2dfd5d
bruno-brn <123398572+bruno-brn@users.noreply.github.com>
2023-12-18 19:41:05 -0300

    adcionado arquivo rota-dashboad

---
+++ backend/app/router/dashboard_router.py
@@ -0,0 +1,66 @@
+from fastapi import APIRouter, Depends, Form, status, UploadFile
+from app.repository.dashboard.dashboard_model import PutDashboard, GetDashboardResponse, RegisterDashboardResponse
+from app.repository.dashboard.dashboard_sqlalchemy import DashboardRepository
+from app.repository.dashboard.dashboard_controller import DashboardController
+from app.repository.dashboard.dashboard_service import DashboardService
+from app.utils.auth import get_authenticated_user
+from sqlalchemy.orm import Session
+from typing import Any, Annotated, Optional
+
+repository = DashboardRepository()
+service = DashboardService(repository)
+controller = DashboardController(service)
+
+router = APIRouter(prefix="/dashboard", tags=["dashboard"])
+
+def get_db(request: Request):
+    return request.state.db
+
+@router.get('/{dashboard_id}', status_code=status.HTTP_200_OK, response_model=GetDashboardResponse)
+def get_dashboard_by_id(dashboard_id: str, current_user: dict = Depends(get_authenticated_user), session: Session = Depends(get_db)) -> Any:
+    return controller.get_dashboard_by_id_controller(dashboard_id, session)
+
+@router.get('/', status_code=status.HTTP_200_OK, response_model=GetDashboardResponse)
+def get_dashboards(current_user: dict = Depends(get_authenticated_user), session: Session = Depends(get_db)) -> Any:
+    return controller.get_dashboards_controller(session)
+
+@router.post('/register', status_code=status.HTTP_201_CREATED, response_model=RegisterDashboardResponse)
+def create_dashboard(
+    name: Annotated[str, Form()],
+    description: Annotated[str, Form()],
+    company_id: Annotated[str, Form()],
+    current_user: dict = Depends(get_authenticated_user),
+    session: Session = Depends(get_db)
+) -> Any:
+    return controller.create_dashboard_controller(
+        name,
+        description,
+        company_id,
+        session
+    )
+
+@router.put('/{dashboard_id}', status_code=status.HTTP_200_OK, response_model=GetDashboardResponse)
+def update_dashboard(
+    dashboard_id: str,
+    name: Optional[str] = Form(None),
+    description: Optional[str] = Form(None),
+    current_user: dict = Depends(get_authenticated_user),
+    session: Session = Depends(get_db)
+) -> Any:
+    return controller.update_dashboard_controller(
+        dashboard_id,
+        name,
+        description,
+        session
+    )
+
+@router.delete('/{dashboard_id}', status_code=status.HTTP_200_OK)
+def delete_dashboard(dashboard_id: str, current_user: dict = Depends(get_authenticated_user), session: Session = Depends(get_db)) -> None:
+    if current_user['role'] == 'admin':
+        return controller.delete_dashboard_controller(dashboard_id, session)
+    else:
+        return GetDashboardResponse(
+            status=False,
+            message='Acesso negado.',
+            data=None,
+        )
