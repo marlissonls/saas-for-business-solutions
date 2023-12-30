835274ac7a17a4a1f3e99234cf43d3873c8493e4
Marlisson Silva <marlisson.business@gmail.com>
2023-12-29 23:17:03 -0300

    refact: rename of files and new interface for predictions

---
+++ backend/app/repository/mlmodels/interfaces/model_interface.py
@@ -0,0 +1,41 @@
+from pydantic import BaseModel
+from typing import Optional, List, Union
+
+class GetModelId(BaseModel):
+    id: str
+
+class PostModel(BaseModel):
+    name: str
+    description: str
+
+class GetModelData(BaseModel):
+    id: str
+    name: str
+    date: str
+    description: str
+    company_id: str
+    features_inputs: Union[str, None]
+    features_template: Union[str, None]
+
+class GetModelResponse(BaseModel):
+    status: bool
+    message: str
+    data: Union[GetModelData, List[GetModelData], None] = None
+
+class PutModel(BaseModel):
+    name: Optional[str] = None
+    description: Optional[str] = None
+
+class RegisterModelResponse(BaseModel):
+    status: bool
+    message: str
+    data: GetModelId | None
+
+class FeatureData(BaseModel):
+    features_values: dict
+    features_template: dict
+
+class PredictResponse(BaseModel):
+    status: bool
+    message: str
+    data: Union[List, int, str, bool, None] = None
