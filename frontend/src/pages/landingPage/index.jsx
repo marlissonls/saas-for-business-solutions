89d5aebb51dac731bbfea82741b9de23e920922b
Marlisson Silva <marlisson.business@gmail.com>
2024-01-01 11:42:13 -0300

    feat: add hero section to landingpage

--- frontend/src/pages/landingPage/index.jsx
+++ frontend/src/pages/landingPage/index.jsx
@@ -6,9 +6,14 @@ const cards = [{id: 1, price: 5, name: '01'}, {id: 2, price: 10, name: '02'}, {i
 function LandingPage(props) {
   return <div className='body'>
     <TopBar />
+
     <div className='hero-section'>
-      Bem Vindos a Insight
+      <h1>Potencialize seu negócio com a Insight!</h1>
+      <p>Descomplicamos seus dados para impulsionar o sucesso do seu negócio!</p>
+      <p>Visualize e analise seus dados de forma dinâmica. Solicite modelos de Machine Learning exclusivos para sua empresa.</p>
+      <p>Conte conosco para transformar informações em ações estratégicas!</p>
     </div>
+
     <div className="cards-container">
       {cards.map(item => <PricingCards price={item.price} name={item.name} key={item.id} />)}
     </div>
