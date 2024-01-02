5a151adef4e1426a57ce9521aa05f21815b0c0f6
Marlisson Silva <marlisson.business@gmail.com>
2024-01-02 15:31:04 -0300

    feat: landingpage index full

--- frontend/src/pages/landingPage/index.jsx
+++ frontend/src/pages/landingPage/index.jsx
@@ -1,22 +1,16 @@
 import TopBar from "./topBar";
-import PricingCards from "./pricingCards";
-
-const cards = [{id: 1, price: 5, name: '01'}, {id: 2, price: 10, name: '02'}, {id: 3, price: 15, name: '03'}]
+import HeroSection from "./heroSection";
+import AboutUsSection from "./aboutUsSection";
+import ServicesSection from "./servicesSection";
+import WhyUsSection from "./whyUsSection";
 
 function LandingPage(props) {
-  return <div className='body'>
+  return <div className='body body-landingpage'>
     <TopBar />
-
-    <div className='hero-section'>
-      <h1>Potencialize seu negócio com a Insight!</h1>
-      <p>Descomplicamos seus dados para impulsionar o sucesso do seu negócio!</p>
-      <p>Visualize e analise seus dados de forma dinâmica. Solicite modelos de Machine Learning exclusivos para sua empresa.</p>
-      <p>Conte conosco para transformar informações em ações estratégicas!</p>
-    </div>
-
-    <div className="cards-container">
-      {cards.map(item => <PricingCards price={item.price} name={item.name} key={item.id} />)}
-    </div>
+    <HeroSection />
+    <AboutUsSection />
+    <ServicesSection />
+    <WhyUsSection />
   </div>
 }
 
