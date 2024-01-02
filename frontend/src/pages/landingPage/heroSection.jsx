e432a765796f09cd3ac94fa0cfa9d635074c3502
Marlisson Silva <marlisson.business@gmail.com>
2024-01-02 15:31:58 -0300

    feat: add heroSection to landingpage

---
+++ frontend/src/pages/landingPage/heroSection.jsx
@@ -0,0 +1,9 @@
+function HeroSection(props) {
+  return <div className='hero-section'>
+    <h1>Insight</h1>
+    <p>Descomplicamos seus dados para impulsionar o sucesso do seu negócio!</p>
+    <p>Crie sua conta!</p>
+  </div>
+}
+
+export default HeroSection;
